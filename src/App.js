3bb4dbca71bd68cd5e8934604708f30000cda44c
sujalsahu <sujalsahu0804@gmail.com>
2022-10-11 21:34:26 +0530

    Last change by sujal sahu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+import {React,useState} from 'react'
 import './App.css';
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Contact from './components/Contact'
+import About from './components/About'
+import Login from './components/Login'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Notestate from './context/notes/notestate';
 
-function App() {
+
+
+function App(){
+  const token=JSON.parse(localStorage.getItem('token'));
+  const [mode,setmode]=useState("dark");
+  const [togglestyle,settogglestyle] = useState({
+    background:"white",
+    color:"black"
+  })
+  const [switchst,setswitchst]=useState({color:"white",
+  background:"#212529"})
+  const togglecolor =()=>{
+    if(mode==="dark"){
+      settogglestyle({  
+        background:"black",
+        color:"white"
+      })
+      setmode("light");
+      setswitchst({
+        color:"#212529",
+        background:"white"
+      })
+      document.body.style.background = "black";
+      document.body.style.color = "white";
+    }
+    else{
+      settogglestyle({
+        background:"white",
+       color:"black"
+      })
+      setmode("dark");
+      setswitchst({
+        color:"white",
+        background:"#212529"
+      })
+     document.body.style.background = "white";
+     document.body.style.color = "black";
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Notestate>
+      <Router>
+     
+      <Routes>
+          <Route exact path="/" element = {<Login/>}></Route>
+          <Route exact path="/home" element = {<Home md={mode} swst={switchst} func={togglecolor} tgst={togglestyle}/>}></Route>
+          <Route exact path="/contact" element={<Contact md={mode} swst={switchst} func={togglecolor} tgst={togglestyle}/>}></Route>
+          <Route exact path="/about" element={<About md={mode} swst={switchst} func={togglecolor} tgst={togglestyle}/>}></Route>
+      </Routes>
+      </Router>
+      </Notestate>
+    </>
   );
 }
 
